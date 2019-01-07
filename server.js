f25b94572f5a7effc5621f546db6b853a3e4aabb
Ivan Petrovic <ipetrovic@Ivans-MacBook-Air.local>
2019-01-06 17:41:15 -0800

    Uncomented code.

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 app.use(logger.log);
 
-app.use((req, res, next) => {
-    res.render('maintanance.hbs');
-});
+// app.use((req, res, next) => {
+//     res.render('maintanance.hbs');
+// });
 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
