59263bbb52456fcef6ec62df567dc9229886d7e8
Ivan Petrovic <ipetrovic@Ivans-MacBook-Air.local>
2019-01-17 13:05:14 -0800

    Added some tests.

--- src/server/test/server.test.js
+++ src/server/test/server.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 const request = require('supertest');
-
+const { ObjectId } = require('mongodb');
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
@@ -10,69 +10,57 @@ beforeEach(done => {
     });
 });
 
-describe('POST /todos', () => {
-    it('should create new todo', (done) => {
-        const text = 'Test todo test';
+// describe('POST /todos', () => {
+//     it('should create new todo', (done) => {
+//         const text = 'Test todo test';
+
+//         request(app)
+//             .post('/todos')
+//             .send({ text })
+//             .expect(200)
+//             .expect((res) => {
+//                 expect(res.body.text).toBe(text);
+//             })
+//             .end((err, res) => {
+//                 if (err) {
+//                     return done(err);
+//                 } else {
+//                     Todo.find().then((todos) => {
+//                         expect(todos.length).toBe(1);
+//                         done();
+//                     }).catch(err => done(err));
+//                 }
+//             });
+//     });
+
+//     it('should not create todo with invalid text.', (done) => {
+//         const text = ' ';
+//         console.log('res');
+//         request(app)
+//             .post('/todos')
+//             .send({})
+//             .expect(400)
+//             .end((err, res) => {
+//                 if (err) {
+//                     return done(err);
+//                 }
+//                 Todo.find().then((todos) => {
+//                     expect(todos.length).toBe(0);
+//                     done();
+//                 }).catch(err => done(err));
+//             });
+//     });
+// });
+
+describe('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
+        const id = 'dsfddswe23';
 
         request(app)
-            .post('/todos')
-            .send({ text })
-            .expect(200)
-            .expect((res) => {
-                expect(res.body.text).toBe(text);
-            })
-            .end((err, res) => {
-                if (err) {
-                    return done(err);
-                } else {
-                    Todo.find().then((todos) => {
-                        expect(todos.length).toBe(1);
-                        done();
-                    }).catch(err => done(err));
-                }
-            });
-    });
+            .get(`/todos/${id}`)
+            .expect(404)
+            .end(done);
 
-    it('should not create todo with invalid text.', (done) => {
-        const text = ' ';
-        console.log('res');
-        request(app)
-            .post('/todos')
-            .send({})
-            .expect(400)
-            .end((err, res) => {
-                if (err) {
-                    return done(err);
-                }
-                Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
-                    done();
-                }).catch(err => done(err));
-            });
-    });
-});
-
-describe('GET /todos', () => {
-    it('should create new todo', (done) => {
-        const text = 'Test todo test';
-
-        request(app)
-            .post('/todos')
-            .send({ text })
-            .expect(200)
-            .expect((res) => {
-                expect(res.body.text).toBe(text);
-            })
-            .end((err, res) => {
-                if (err) {
-                    return done(err);
-                } else {
-                    Todo.find().then((todos) => {
-                        expect(todos.length).toBe(1);
-                        done();
-                    }).catch(err => done(err));
-                }
-            });
     });
 
 
